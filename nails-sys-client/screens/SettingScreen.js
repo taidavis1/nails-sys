3b5b8edb6b8e70b013b2b0e5a855d5f40bd8ccb6
victhangnguyen <victhangnguyen@outlook.com>
2022-01-10 05:18:35 +0700

    2022-01-10 -> update Screen Setting, SignIn, SignUp, Drawer Navigator

---
+++ nails-sys-client/screens/SettingScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SettingScreen = () => {
+
+    return (
+        <View>
+            <Text>
+                Hello Setting Screen.
+            </Text>
+        </View>
+    );
+};
+
+export default SettingScreen;
