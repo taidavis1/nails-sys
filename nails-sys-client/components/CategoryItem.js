e90e1993c7a5ad3aa381fe01152a798945efe1fa
victhangnguyen <victhangnguyen@outlook.com>
2022-01-26 08:40:31 +0700

    recode ServiceScreen

--- nails-sys-client/components/CategoryItem.js
+++ nails-sys-client/components/CategoryItem.js
@@ -4,17 +4,17 @@ import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-nati
 import Colors from '../constants/color';
 import Grid from '../constants/layout/grid';
 
-const CategoryItem = (props) => {
+const CategoryItem = ({ item, empty, style }) => {
     const touchHandler = () => {};
     return (
         <TouchableOpacity
             activeOpacity={0.5}
             onPress={touchHandler}
-            style={!props.empty ? [styles.item, props.style] : [styles.item, styles.itemInvisible, props.style]}
+            style={!empty ? [styles.item, style] : [styles.item, styles.itemInvisible, style]}
         >
             <View>
-                <Text style={styles.itemText}>{!props.empty && props.item.id && props.item.id}</Text>
-                <Text style={styles.itemText}>{!props.empty && props.item.username && props.item.username}</Text>
+                <Text style={styles.itemText}>{!empty && item.id && item.id}</Text>
+                <Text style={styles.itemText}>{!empty && item.username && item.username}</Text>
             </View>
         </TouchableOpacity>
     );
@@ -23,8 +23,9 @@ const CategoryItem = (props) => {
 const styles = StyleSheet.create({
     screen: {},
     item: {
-        width: Dimensions.get('window').width / Grid.numColumns,
-        height: Dimensions.get('window').width / Grid.numColumns,
+
+        width: (Dimensions.get('window').width * 65 / 100 - 50) / 4,
+        height: (Dimensions.get('window').width * 40 / 100 - 50) / 4,
         // flex: 1,
         backgroundColor: Colors.brandLight,
         alignItems: 'center',
--- nails-sys-client/screens/ServicesScreen.js
+++ nails-sys-client/screens/ServicesScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions, ScrollView } from 'react-native';
 //! API
 import axios from 'axios';
 //! comps
-import CategoryItem from '../components/CategoryItem';
+// import CategoryItem from '../components/CategoryItem';
+import CategorySectionList from '../components/CategorySectionList';
+
 //! hooks
 import useScreenDimensions from '../hooks/useScreenDimensions';
 
@@ -12,102 +14,90 @@ const CategoryScreen = (props) => {
     const numColumns = React.useRef(3);
     const [gutter, setGutter] = React.useState(10);
 
-    const [categories, setCategories] = React.useState([]);
-    const isMounted = React.useRef(true);
     const screenHeight = Dimensions.get('window').height;
     // console.log(`screenWidth: `, screenWidth);
     // console.log(`screenHeight: `, screenHeight);
 
     const screenData = useScreenDimensions();
-    const screenWidth = Dimensions.get('screen').width * 65 / 100;
+    const screenWidth = Dimensions.get('screen').width;
 
-    const getCategoryData = (screenWidth) => {
-        let numColumns = 3;
-        let gutter = 10;
-        let categoryWidth = (screenWidth - (numColumns + 1) * gutter) / numColumns;
+    const categoryData = [
+        {
+            categoryName: 'ENHANCEMENT',
+            categoryColor: '#141466',
+            categoryScreen: 'EnhancementScreen',
+            categoryURI: 'https://jsonplaceholder.typicode.com/users',
+        },
+        {
+            categoryName: 'PEDI/MANI',
+            categoryColor: '#972727',
+            categoryScreen: 'PediManiScreen',
+            categoryURI: 'https://jsonplaceholder.typicode.com/users',
+        },
+        {
+            categoryName: 'WAXING',
+            categoryColor: '#6e12cb',
+            categoryScreen: 'WaxingScreen',
+            categoryURI: 'https://jsonplaceholder.typicode.com/users',
+        },
+        {
+            categoryName: "KID's",
+            categoryColor: '#9f7a34',
+            categoryScreen: 'KidsScreen',
+            categoryURI: 'https://jsonplaceholder.typicode.com/users',
+        },
+        {
+            categoryName: 'CATEGORY5',
+            categoryColor: '#28675b',
+            categoryScreen: 'Category5',
+            categoryURI: 'https://jsonplaceholder.typicode.com/users',
+        },
+    ];
 
-        if (screenWidth >= 768) {
-            numColumns = 5;
-            gutter = 10;
-            categoryWidth = (screenWidth - (numColumns + 1) * gutter) / numColumns;
-        }
-        if (screenWidth >= 1024) {
-            numColumns = 7;
-            gutter = 10;
-            categoryWidth = (screenWidth - (numColumns + 1) * gutter) / numColumns;
-        }
-        //!iPad Air : 1180 x 820
-        return { numColumns: numColumns, gutter: gutter, categoryWidth: categoryWidth };
-    };
+    // const getCategoryData = (screenWidth) => {
+    //     let numColumns = 3;
+    //     let gutter = 10;
+    //     let categoryWidth = (screenWidth - (numColumns + 1) * gutter) / numColumns;
 
-    React.useEffect(() => {
-        return () => {
-            isMounted.current = false;
-        };
-    }, []);
+    //     if (screenWidth >= 768) {
+    //         numColumns = 5;
+    //         gutter = 10;
+    //         categoryWidth = (screenWidth - (numColumns + 1) * gutter) / numColumns;
+    //     }
+    //     if (screenWidth >= 1024) {
+    //         numColumns = 7;
+    //         gutter = 10;
+    //         categoryWidth = (screenWidth - (numColumns + 1) * gutter) / numColumns;
+    //     }
+    //     //!iPad Air : 1180 x 820
+    //     return { numColumns: numColumns, gutter: gutter, categoryWidth: categoryWidth };
+    // };
 
-    React.useEffect(() => {
-        // Dimensions.addEventListener('change', () => adaptLayout)
-        if (isMounted.current) {
-            setCategoryWidth(() => getCategoryData(screenWidth).categoryWidth);
-            // numColumns.current(() => getCategoryData(screenWidth).numColumns);
-            setGutter(() => getCategoryData(screenWidth).gutter);
-        }
-    }, [screenWidth]);
-    // const adaptLayout = (dimensions) => {
-    //     setCategoryWidth(dimensions.window.width)
-    // }
+    // const formatData = (data, numColumns) => {
+    //     const numberOfFullRows = Math.floor(data.length / numColumns);
+    //     let numberOfElementLastRows = data.length - numberOfFullRows * numColumns;
+    //     while (numberOfElementLastRows !== 0 && numberOfElementLastRows !== numColumns) {
+    //         //! add [element data except index and separators]
+    //         //! username and id
+    //         data.push({ username: `blank-${numberOfElementLastRows}`, empty: true });
+    //         numberOfElementLastRows += 1;
+    //     }
+    //     return data;
+    // };
 
-    const getCategories = async () => {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-        return response;
-    };
-
-    const loadCategories = async () => {
-        await getCategories()
-            .then((res) => {
-                if (isMounted.current) {
-                    setCategories(res.data);
-                }
-            })
-            .catch((error) => console.log(`error: `, error));
-    };
-
-    React.useEffect(() => {
-        const unsubscribe = loadCategories();
-        return unsubscribe;
-    }, []);
-
-    const formatData = (data, numColumns) => {
-        const numberOfFullRows = Math.floor(data.length / numColumns);
-        let numberOfElementLastRows = data.length - numberOfFullRows * numColumns;
-        while (numberOfElementLastRows !== 0 && numberOfElementLastRows !== numColumns) {
-            //! add [element data except index and separators]
-            //! username and id
-            data.push({ username: `blank-${numberOfElementLastRows}`, empty: true });
-            numberOfElementLastRows += 1;
-        }
-        return data;
-    };
-
-    const CategoryItemWithEmpty = (itemData) => {
-        if (itemData.item.empty === true) {
-            return <CategoryItem item={itemData.item} empty={true} style={{ width: categoryWidth, height: categoryWidth }} />;
-        }
-        return <CategoryItem item={itemData.item} style={{ width: categoryWidth, height: categoryWidth }} />;
-    };
+    // const CategoryItemWithEmpty = (itemData) => {
+    //     if (itemData.item.empty === true) {
+    //         return <CategoryItem item={itemData.item} empty={true} style={{ width: categoryWidth, height: categoryWidth }} />;
+    //     }
+    //     return <CategoryItem item={itemData.item} style={{ width: categoryWidth, height: categoryWidth }} />;
+    // };
 
     return (
-        <View style={styles.screen}>
-            <FlatList
-                keyExtractor={(item) => item.id}
-                data={formatData(categories, screenWidth > 1024 ? 7 : screenWidth > 768 ? 5 : 3)}
-                // data={categories}
-                style={styles.container}
-                renderItem={(itemData) => CategoryItemWithEmpty(itemData)}
-                numColumns={screenWidth > 1024 ? 7 : screenWidth > 768 ? 5 : 3}
-            />
-        </View>
+        <ScrollView horizontal={true} style={styles.screen}>
+            {categoryData.map((data, index) => {
+                return <CategorySectionList key={index} data={data} />;
+            })}
+        </ScrollView>
     );
 };
 
